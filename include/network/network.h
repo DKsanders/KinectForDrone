db04377931aa5fe53c6c3ecd4d9043bcabe036bd
David <sander57@ubuntu.(none)>
2014-08-12 17:42:01 -0400

    implemented ByteStream class

--- include/network/network.h
+++ include/network/network.h
@@ -17,9 +17,64 @@
 #include <cstring>
 
 #define MAX_BUF_SIZE 1024
+#define DEFAULT_BUF_SIZE 1024
 #define CONNECTION_WAIT_TIME 5 // Time between connection attempts made by client
 
 using namespace std;
+
+// Byte Stream for easy reading/writing
+class ByteStream {
+public:
+    ByteStream();
+    ByteStream(int _bufSize);
+    ByteStream(const char* data, int dataSize);
+    ByteStream(int _bufSize, const char* data, int dataSize);
+    ~ByteStream();
+
+    // Accessors
+    int getWriteIndex();
+    int getReadIndex();
+    int getBufSize();
+    char* getBuf();
+
+    /**
+     * Writes data to a byte stream in LITTLE ENDIAN
+     * Arguments:
+     *  buf(OUTPUT) - byte stream
+     *  data(INPUT) - data written to byte stream
+     * Return:
+     *  number of bytes read from byte stream; 0 if failed
+     */
+    int write(int data);
+    int write(float data);
+    int write(double data);
+    int write(const string& data); // Copies every character until '\0' copied
+    int write(const char* data); // Copies every character until '\0' copied
+    int write(const char* data, int length); // Copies the number of characters specified by length
+
+    /**
+     * Reads data from a byte stream in LITTLE ENDIAN
+     * Arguments:
+     *  buf(INPUT) - byte stream
+     *  data(OUTPUT) - data written to byte stream
+     * Return:
+     *  number of bytes read from byte stream; 0 if failed
+     */
+    int read(int& data);
+    int read(float& data);
+    int read(double& data);
+    int read(string& data);
+    int read(char* data, int length);
+
+    void clear(); // clears buffer
+    void clear(int _bufSize); // clears buffer
+
+private:
+    int writeIndex; // Keeps track of where to write to in buffer
+    int readIndex; // Keeps track of where to read from in buffer
+    int bufSize;
+    char* buf;
+};
 
 // Base of server
 class Server{
@@ -127,9 +182,9 @@ protected:
 int write2stream(char* buf, int data);
 int write2stream(char* buf, float data);
 int write2stream(char* buf, double data);
+int write2stream(char* buf, const string& data); // Copies every character until '\0' copied
 int write2stream(char* buf, const char* data); // Copies every character until '\0' copied
 int write2stream(char* buf, const char* data, int length); // Copies the number of characters specified by length
-
 
 /**
  * Reads data from a byte stream in LITTLE ENDIAN
--- src/network.cpp
+++ src/network.cpp
@@ -9,6 +9,226 @@
 #include <endian.h>
 #include <stdint.h>
 
+ByteStream::ByteStream() {
+	bufSize = DEFAULT_BUF_SIZE;
+	buf = new char[bufSize];
+	writeIndex = 0;
+	readIndex = 0;
+}
+
+ByteStream::ByteStream(int _bufSize) {
+	bufSize = _bufSize;
+	buf = new char[bufSize];
+	writeIndex = 0;
+	readIndex = 0;
+}
+
+ByteStream::ByteStream(const char* data, int dataSize) {
+	bufSize = DEFAULT_BUF_SIZE;
+	buf = new char[bufSize];
+	writeIndex = dataSize;
+	readIndex = 0;
+	memcpy(buf, data, dataSize);
+}
+
+ByteStream::ByteStream(int _bufSize, const char* data, int dataSize) {
+	bufSize = _bufSize;
+	buf = new char[bufSize];
+	writeIndex = dataSize;
+	readIndex = 0;
+	memcpy(buf, data, dataSize);
+}
+
+ByteStream::~ByteStream(){
+	delete [] buf;
+}
+
+int ByteStream::getWriteIndex(){
+	return writeIndex;
+}
+
+int ByteStream::getReadIndex(){
+	return readIndex;
+}
+
+int ByteStream::getBufSize(){
+	return bufSize;
+}
+
+char* ByteStream::getBuf(){
+	return buf;
+}
+
+int ByteStream::write(int data) {
+	// Check size (should be 4 bytes)
+	int size = sizeof(int);
+	if((writeIndex + size) > bufSize){
+		// Not enough room in buffer
+		return 0;
+	}
+	if(size == 4){
+		// As expected
+		uint32_t bytes = htole32(*(uint32_t*)(&data));
+		memcpy(buf+writeIndex, &bytes, size);
+	} else {
+		// not standard size, don't write
+		return 0;
+	}
+	writeIndex += size;
+	return size;
+}
+
+int ByteStream::write(float data) {
+	// Check size (should be 4 bytes)
+	int size = sizeof(float);
+	if((writeIndex + size) > bufSize){
+		// Not enough room in buffer
+		return 0;
+	}
+	if(size == 4){
+		// As expected
+		uint32_t bytes = htole32(*(uint32_t*)(&data));
+		memcpy(buf+writeIndex, &bytes, size);
+	} else {
+		// not standard size, don't write
+		return 0;
+	}
+	writeIndex += size;
+	return size;
+}
+
+int ByteStream::write(double data) {
+	// Check size (should be 8 bytes)
+	int size = sizeof(double);
+	if((writeIndex + size) > bufSize){
+		// Not enough room in buffer
+		return 0;
+	}
+	if(size == 8){
+		// As expected
+		uint64_t bytes = htole64(*(uint64_t*)(&data));
+		memcpy(buf+writeIndex, &bytes, size);
+	} else {
+		// not standard size, don't write
+		return 0;
+	}
+	writeIndex += size;
+	return size;
+}
+
+int ByteStream::write(const string& data) {
+	return this->write(data.c_str());
+}
+
+int ByteStream::write(const char* data) {
+	// Write an entire string (including "\0")
+	int size = strlen(data)+1;
+	if((writeIndex + size) > bufSize){
+		// Not enough room in buffer
+		return 0;
+	}
+	return this->write(data, size);
+}
+
+int ByteStream::write(const char* data, int length) {
+	// Write specified length
+	if((writeIndex + length) > bufSize){
+		// Not enough room in buffer
+		return 0;
+	}
+	memcpy(buf+writeIndex, data, length);
+	writeIndex += length;
+	return length;
+}
+
+int ByteStream::read(int& data){
+	// Check size (should be 4 bytes)
+	int size = sizeof(int);
+	if((readIndex + size) > bufSize){
+		// Not enough room in buffer
+		return 0;
+	}
+	if(size == 4){
+		// As expected
+		uint32_t temp = le32toh(*(uint32_t*)(buf+readIndex));
+		data = *((int*)(&temp));
+	} else {
+		// not standard size, don't write
+		return 0;
+	}
+	readIndex += size;
+	return size;
+}
+
+int ByteStream::read(float& data){
+	// Check size (should be 4 bytes)
+	int size = sizeof(float);
+	if((readIndex + size) > bufSize){
+		// Not enough room in buffer
+		return 0;
+	}
+	if(size == 4){
+		// As expected
+		uint32_t temp = le32toh(*(uint32_t*)(buf+readIndex));
+		data = *((float*)(&temp));
+	} else {
+		// not standard size, don't write
+		return 0;
+	}
+	readIndex += size;
+	return size;
+}
+
+int ByteStream::read(double& data){
+	// Check size (should be 4 bytes)
+	int size = sizeof(double);
+	if((readIndex + size) > bufSize){
+		// Not enough room in buffer
+		return 0;
+	}
+	if(size == 8){
+		// As expected
+		uint64_t temp = le64toh(*(uint64_t*)(buf+readIndex));
+		data = *((double*)(&temp));
+	} else {
+		// not standard size, don't write
+		return 0;
+	}
+	readIndex += size;
+	return size;
+}
+
+int ByteStream::read(string& data){
+	// Read all chars until '\0'
+	string temp(buf+readIndex);
+	data = temp;
+	int size = data.length()+1;
+	readIndex += size;
+	return size;
+}
+
+int ByteStream::read(char* data, int length){
+	if((readIndex + length) > bufSize){
+		// Not enough room in buffer
+		return 0;
+	}
+	memcpy(data, buf+readIndex, length);
+	readIndex += length;
+	return length;
+}
+
+void ByteStream::clear(){
+	this->clear(DEFAULT_BUF_SIZE);
+}
+
+void ByteStream::clear(int _bufSize){
+	delete [] buf;
+	bufSize = _bufSize;
+	buf = new char[bufSize];
+	writeIndex = 0;
+	readIndex = 0;
+}
+
 int write2stream(char* buf, int data){
 	// Check size (should be 4 bytes)
 	int size = sizeof(int);
@@ -66,7 +286,7 @@ int write2stream(char* buf, double data) {
 int write2stream(char* buf, const char* data) {
 	// Write an entire string (including "\0")
 	return write2stream(buf, data, strlen(data)+1);
-};
+}
 
 int write2stream(char* buf, const char* data, int length) {
 	// Write specified length
